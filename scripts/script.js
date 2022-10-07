057b010466de3692cf35742c09598b7b9d7bdc4a
kduran <kduranmalave@gmail.com>
2022-10-07 18:14:11 -0300

    agregando arrays

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ let passReg = prompt('Crea la contraseña de ingreso');
 
 
 function Login(){
-
+    let login = false
     while (passReg.length<8) {
         alert('La contraseña debe contener al menos 8 caracteres')
         let newPassReg = prompt('ingresa nuevamente la contraseña')
@@ -23,8 +23,8 @@ function Login(){
                             login = true;
                             break;} 
                     } else if (userName != userReg || userPass != newPassReg) {
-                        alert ('nombre de usuario o contraseña incorrecta, por favor ingresala nuevamente te quedan ' + i + ' intentos');}
-                }
+                        alert ('nombre de usuario o contraseña incorrecta, por favor ingresala nuevamente te quedan ' + i + ' intentos');} 
+                    }
             }    
         }
     alert ('A continuacion te vamos a solicitar ingresar tus datos registrados, ten en cuenta que solo contaras con 3 intentos para ingresarlos correctamente')
@@ -41,37 +41,30 @@ function Login(){
             alert ('nombre de usuario o contraseña incorrecta, por favor ingresala nuevamente te quedan ' + i + ' intentos');}
     }
 
-let login = false;
+
 }
 
 Login ()
 
+function comic(Coname, ID, year, dateAgg, editorial) {
+    this.name = Coname;
+    this.year = year;
+    this.ID = ID;
+    this.date = dateAgg;
+    this.editorial = editorial;
+}
 
+const Comic1 = {
+    Coname : 'THE AMAZING SPIDERMAN FULL CIRCLE',
+    year : 2000,
+    ID: 10,
+    dateAgg: new Date("November 28, 2021 10:00:00"),
+    editorial: 'MARVEL COMICS',
+}
+const Comic2 = new comic('THE BOYS VOL. 1', 2002, 7, new Date("December 25, 2021 13:30:00"), 'UTOPIA EDITORIAL')
+const Comic3 = new comic('NIGHTWING REBIRTH', 2010, 15, new Date("March 10, 2022 15:40:00"), 'DC COMICS')
+const Comic4 = new comic('LIFE IS STRANGE', 2015, 21, new Date("March 10, 2022 16:10:00"), 'HEROES DE PAPEL')
 
-
-
-
-
-// if(condicion){
-
-//     console.log('la condicion es verdadera')
-
-// }
-
-// if (edad==37){
-//     console.log('La edad es 37')
-// }
-
-// if (edad===string) {
-//     console.log('Son iguales')
-// }
-
-
-// if (edadUser>=edadObligatoria && edadUser<=edadMaxima){
-//     alert('Debes votar')
-// } else if (edadUser>=edadOptativa || edadUser>=edadMaxima) {
-//     alert('Puedes votar si quieres')
-// } else {
-//     alert('No debes votar')
-// }
-
+const comicList = []
+comicList.push(Comic1,Comic2,Comic3,Comic4)
+console.log (comicList)
