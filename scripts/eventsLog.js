9129b373557e3c1a72b78974081da5364645e522
kduran <kduranmalave@gmail.com>
2022-10-25 20:09:14 -0300

    se logra

--- scripts/eventsLog.js
+++ scripts/eventsLog.js
@@ -5,8 +5,8 @@ const regName = document.getElementById("validationCustom01"),
   regPassword = document.getElementById("validationCustom05"),
   register = document.getElementById("register"),
   recordar = document.getElementById("recordarme"),
-  userLogin = document.getElementById('userLogin'),
-  passLogin = document.getElementById('passLogin'),
+  userLogin = document.getElementById("userLogin"),
+  passLogin = document.getElementById("passLogin"),
   form = document.querySelectorAll('input[type="text"]'),
   modalEl = document.getElementById("modalLogin"),
   modal = new bootstrap.Modal(modalEl),
@@ -21,11 +21,8 @@ function persona(nombre, apellido, nombreUsuario, contraseña) {
     (this.contraseña = contraseña);
 }
 
-
-
 function usuario(username, password) {
-  (this.username=username),
-  (this.password=password);
+  (this.username = username), (this.password = password);
 }
 
 const btnLog = document.getElementById("login");
@@ -57,79 +54,79 @@ let Capturar = function () {
     console.log(userList);
     guardarDatos();
   }
-
-
 };
 register.addEventListener("click", (e) => {
   e.preventDefault();
   Capturar();
-
 });
-
 
 btnLog.onclick = (e) => {
   e.preventDefault();
-  beginLog();
+  validacion();
   modal.hide();
 };
+const usuario1 = new usuario(userLogin.value, passLogin.value);
 
 function beginLog() {
-  const usuario1 = new usuario(
-    userLogin.value,
-    passLogin.value,
-  );
-  let data = validacion(userList, regUserName.value, regPassword.value);
+  getUser();
+  let data = (userList[0].nombreUsuario,userLogin.value,passLogin.value);
   if (!data) {
-    alert('Usuario y/o contraseña erróneos');} else {
-      if (recordar.checked) {
-          guardarDatos(data, localStorage);
-          saludar(recuperarUsuario(localStorage));
-      } else {
-          guardarDatos(data, sessionStorage);
-          saludar(recuperarUsuario(sessionStorage));
-      }
-      //Recién ahora cierro el cuadrito de login
-      modal.hide();
+    alert("Usuario y/o contraseña erróneos");
+  } else {
+    if (recordar.checked) {
+      guardarDatos(userLogin.value, localStorage);
+      saludar(getUser(localStorage));
+    } else {
+      guardarDatos(userLogin.value, sessionStorage);
+      saludar(getUser(sessionStorage));
+    }
+  }
+  modal.hide();
+  presentarInfo(toggles, 'd-none');
 }
 
-function validacion(usersArray, user, pass) {
-  let encontrado = usersArray.find((userDB) => userDB.nombreUsuario == user);
-
-    if (typeof encontrado === 'undefined') {
-        return false;
-    } else {
-        if (encontrado.pass != pass) {
-            return false;
-        } else {
-            return encontrado;
-        }
+function validacion() {
+  if (userLogin.value != userList[0].nombreUsuario || passLogin.value != userList[0].contraseña) {
+    alert("Usuario y/o contraseña erróneos");
+    return false;
+  }  else{ if (recordar.checked) {
+        guardarDatos(userLogin.value, localStorage);
+        saludar(getUser(localStorage));
+      } else {
+      console.log("datos ingresados correctamente");
+      guardarDatos(userLogin.value, sessionStorage);
+      saludar(getUser(sessionStorage));
+      userLogged();
+      return true;
     }
 }
 }
+function presentarInfo(array, clase) {
+  array.forEach(element => {
+      element.classList.toggle(clase);
+  });
+}
 
-function userLogged(userList) {
-  if (userList) {
+function userLogged(usuario) {
+  if (usuario) {
     saludar();
     presentarInfo(toggles, "d-none");
   }
 }
 
-
-
-function saludar(persona) {
-  nombreUsuario.innerHTML = `Bienvenido/a, <span>${persona.nombre}</span>`;
+function saludar(usuario) {
+  nombreUsuario.innerHTML = `Bienvenido/a, <span>${"usuario".nombre}</span>`;
 }
 
-function guardarDatos(persona) {
+function guardarDatos(usuario) {
   localStorage.setItem("usuario", JSON.stringify(userList));
 }
-function getUser(user) {
-  let usuarioReg = JSON.parse(localStorage.getItem('user'));
-  if (user == null) {
+function getUser(usuario) {
+  let usuarioReg = JSON.parse(localStorage.getItem("usuario"));
+  if (usuario == null) {
     return false;
   } else {
-    console.log(usuarioReg)
+    console.log(usuarioReg);
     return usuarioReg;
   }
 }
-
